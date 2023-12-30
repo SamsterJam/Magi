59c5ea70d416ba734499799f0cc92f88c8c32d30
SamsterJam <samsterjam@gmail.com>
2023-12-29 20:42:12 -0500

    Refactored code to use Porcupine wake-word for faster response times

--- main.py
+++ main.py
@@ -2,43 +2,41 @@ import os
 import re
 import datetime
 import time
-from threading import Thread
+import numpy as np
+import sounddevice as sd
 from queue import Queue
+from threading import Thread
 from colorama import Fore, Style, init
 import speech_recognition as sr
 from google.cloud import texttospeech
-from scipy.io import wavfile
-import sounddevice as sd
 from dotenv import load_dotenv
 from openai import OpenAI
+import pvporcupine
+from scipy.io import wavfile
 
 # Load environment variables from a .env file
 load_dotenv()
 
 # Global options
-KEY_WORD = "Magi"
+KEY_WORD = "Hey Magi"  # Replace with the actual keyword you have a model for
 SPEAKING_RATE = 1.15
 VOICE_NAME = "en-US-Polyglot-1"
 PITCH = -5.0
-SYSTEM_INSTRUCTIONS_FILE = 'system.txt'
 OUTPUT_AUDIO_FILE = 'output.wav'
 NOISE_CALIBRATION_TIME = 5
+COMMAND_AWAIT_TIME_OUT = 10
 
-REPLACEMENTS = {
-    "Magic": KEY_WORD,
-    "madre": KEY_WORD,
-    "Madre": KEY_WORD,
-    "Madge": KEY_WORD,
-    "nanjai" : KEY_WORD,
-    "Manjai" : KEY_WORD,
-}
+RECORDINGS_DIR = 'recordings'
+if not os.path.exists(RECORDINGS_DIR):
+    os.makedirs(RECORDINGS_DIR)
 
 # Set up your API keys
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 GOOGLE_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+PORCUPINE_ACCESS_KEY = os.getenv('PORCUPINE_ACCESS_KEY')
 
 # Initialize OpenAI client
-client = OpenAI(api_key=OPENAI_API_KEY)
+openai_client = OpenAI(api_key=OPENAI_API_KEY)
 
 # Initialize Google Text-to-Speech client
 tts_client = texttospeech.TextToSpeechClient.from_service_account_json(GOOGLE_CREDENTIALS)
@@ -55,11 +53,9 @@ recognizer = sr.Recognizer()
 # Initialize Conversation History
 conversation_history = []
 
-# Load in System Instructions
-with open(SYSTEM_INSTRUCTIONS_FILE, 'r') as file:
-    system_content = file.read()
+# Initialize Noise Threshold
+ambient_noise_energy_threshold = None
 
-conversation_history.append({"role": "system", "content": system_content})
 
 # Custom logging function with timestamps and keyword highlighting
 def log(message, highlight=False):
@@ -70,22 +66,101 @@ def log(message, highlight=False):
         message = re.sub(pattern, f"{Fore.GREEN}{KEY_WORD}{Style.RESET_ALL}{Fore.LIGHTBLACK_EX}", message, flags=re.IGNORECASE)
     print(f"{Fore.LIGHTBLACK_EX}[{timestamp}] {message}{Style.RESET_ALL}")
 
-# Function to query OpenAI's GPT using the Conversation API
-def ask_openai(question):
-    conversation_history.append({"role": "user", "content": question})
+
+def play_acknowledgment_sound(ack_sound_file):
     try:
-        response = client.chat.completions.create(
-            model="gpt-3-turbo-1106",
+        # Read the audio file
+        fs, data = wavfile.read(ack_sound_file)
+        # Play the audio file
+        sd.play(data, fs)
+    except Exception as e:
+        log(f"Error playing acknowledgment sound: {e}")
+
+
+def calibrate_for_ambient_noise():
+    global ambient_noise_energy_threshold
+    with sr.Microphone() as source:
+        log(f"Calibrating for ambient noise ({NOISE_CALIBRATION_TIME}s)... ")
+        recognizer.adjust_for_ambient_noise(source, duration=NOISE_CALIBRATION_TIME)
+        ambient_noise_energy_threshold = recognizer.energy_threshold
+        log(f"Calibrated energy threshold: {ambient_noise_energy_threshold}")
+
+
+
+
+# Initialize Porcupine wake word engine
+porcupine = None
+keyword_path = os.path.join(os.path.dirname(__file__), "Magi-wake.ppn")
+
+def listen_for_wake_word():
+    global porcupine
+    porcupine = pvporcupine.create(access_key=PORCUPINE_ACCESS_KEY, keyword_paths=[keyword_path])
+
+    def callback(indata, frames, time, status):
+        if status:
+            log(f"Error: {status}")
+        # Convert the input data to 16-bit integers
+        if indata.dtype != np.int16:
+            indata = (indata * 32767).astype(np.int16)
+        keyword_index = porcupine.process(indata.flatten())
+        if keyword_index >= 0:
+            log(f"Keyword '{KEY_WORD}' detected, listening for command...")
+            # Play the acknowledgment sound
+            play_acknowledgment_sound('acknowledgment.wav')
+            speech_queue.put(True)  # Signal that the wake word was detected
+
+    with sd.InputStream(callback=callback,
+                        blocksize=porcupine.frame_length,
+                        samplerate=porcupine.sample_rate,
+                        channels=1):
+        log(f"Listening for keyword '{KEY_WORD}'...")
+        while True:
+            time.sleep(0.1)
+
+
+def recognize_speech():
+    with sr.Microphone() as source:
+        log("Please speak your command...")
+        recognizer.energy_threshold = ambient_noise_energy_threshold
+        try:
+            audio = recognizer.listen(source, timeout=COMMAND_AWAIT_TIME_OUT)
+
+            text = recognizer.recognize_google(audio)
+            log("Processed Audio: " + text)
+
+            # Save the audio data to a file in the 'recordings' directory
+            timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            audio_filename = os.path.join(RECORDINGS_DIR, f"audio_{timestamp}.wav")
+            with open(audio_filename, "wb") as audio_file:
+                audio_file.write(audio.get_wav_data())
+            log(f"Audio saved to {audio_filename}")
+            
+            return text
+        except sr.WaitTimeoutError:
+            log("No speech was detected within the timeout period.")
+        except sr.UnknownValueError:
+            log("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            log(f"Could not request results from Google Speech Recognition service; {e}")
+    return None
+
+
+def process_command(command):
+    conversation_history.append({"role": "user", "content": command})
+    try:
+        response = openai_client.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
             messages=conversation_history
         )
         assistant_reply = response.choices[0].message.content
         conversation_history.append({"role": "assistant", "content": assistant_reply})
+        log("Assistant Response: " + assistant_reply)
         return assistant_reply
     except Exception as e:
         log(f"Error querying OpenAI: {e}")
     return "I'm sorry, I can't process your request right now."
 
-# Function to synthesize speech using Google TTS
+
 def synthesize_speech(text):
     synthesis_input = texttospeech.SynthesisInput(text=text)
     voice = texttospeech.VoiceSelectionParams(
@@ -109,69 +184,40 @@ def synthesize_speech(text):
         log(f"Error synthesizing speech: {e}")
     return None
 
-# Function to play the synthesized speech
+
+
 def play_speech(audio_content):
     if audio_content:
         with open(OUTPUT_AUDIO_FILE, 'wb') as out:
             out.write(audio_content)
         fs, data = wavfile.read(OUTPUT_AUDIO_FILE)
         log("Playing speech...")
-        sd.play(data, fs, latency=0.1)
+        sd.play(data, fs)
         sd.wait()
-
-# Function to recognize speech with keyword highlighting using listen_in_background
-def listen_for_speech_in_background():
-    def callback(recognizer, audio):
-        try:
-            text = recognizer.recognize_google(audio)
-            for word, replacement in REPLACEMENTS.items():
-                text = re.sub(r'\b' + re.escape(word) + r'\b', replacement, text)
-            log(f"Recognizer heard: {text}", highlight=KEY_WORD in text)
-            if KEY_WORD in text:
-                speech_queue.put(text)
-        except sr.UnknownValueError:
-            log("Could not understand audio")
-        except sr.RequestError as e:
-            log(f"Could not request results; {e}")
-
-    # Create a microphone source and adjust for ambient noise
-    with sr.Microphone() as source:
-        log(f"Calibrating for Noise ({NOISE_CALIBRATION_TIME}s)...")
-        recognizer.adjust_for_ambient_noise(source, duration=NOISE_CALIBRATION_TIME)
-
-    # Now that we've adjusted for ambient noise, start listening in the background
-    log("Listening in background...")
-    stop_listening = recognizer.listen_in_background(sr.Microphone(), callback)
-    return stop_listening
-
-
-def process_speech_queue():
-    while True:
-        text = speech_queue.get()
-        log(f"Keyword detected!")
-        response = ask_openai(text)
-        log(f"Assistant says: {response}")
-        audio_content = synthesize_speech(response)
-        play_speech(audio_content)
-        speech_queue.task_done()
 
 
 def main():
-    stop_listening = listen_for_speech_in_background()
-    worker_thread = Thread(target=process_speech_queue)
-    worker_thread.daemon = True
-    worker_thread.start()
+    wake_word_thread = Thread(target=listen_for_wake_word)
+    wake_word_thread.daemon = True
+    wake_word_thread.start()
 
     try:
-        # Keep the main thread alive
         while True:
-            # You can add a small sleep here to reduce CPU usage
-            time.sleep(0.1)
+            # Wait for the wake word to be detected
+            speech_queue.get()
+            command = recognize_speech()
+            if command:
+                play_acknowledgment_sound('acknowledgment.wav')
+                response = process_command(command)
+                audio_content = synthesize_speech(response)
+                play_speech(audio_content)
+            speech_queue.task_done()
     except KeyboardInterrupt:
-        # Stop listening when the user presses Ctrl+C
-        stop_listening(wait_for_stop=False)
-        log("Stopped listening")
-        worker_thread.join()  # Wait for the worker thread to finish processing
+        log("Exiting program...")
+    finally:
+        if porcupine is not None:
+            porcupine.delete()
 
 if __name__ == "__main__":
+    calibrate_for_ambient_noise()
     main()
